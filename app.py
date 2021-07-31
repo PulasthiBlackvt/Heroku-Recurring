0a7bcef47b9e918e3f41861d63e3d15e172bfa59
PulaGitLab <pulasthi.a@blackvt.com>
2021-07-31 20:01:45 +0530

    flask app

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 # Create flask app
-app = Flask(__name__)
+flask_app = Flask(__name__)
 model = pickle.load(open("model.pkl", "rb"))
 
 @flask_app.route("/")
